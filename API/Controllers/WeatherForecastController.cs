5dc40e792ab6ec0506163e657840bee0008a2231
NuclMe <saltansergei@gmail.com>
2024-11-27 01:18:44 +0200

    created API

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ namespace API.Controllers;
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
-    {
+    {   
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
---
+++ API/Enteties/AppUser.cs
@@ -0,0 +1,6 @@
+namespace API.Entities;
+public class AppUser
+{
+    public int id { get; set; } = 0;
+    public required string UserName { get; set; }
+}
