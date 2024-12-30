a94a60176c85436a3aaf10dd52520d0ddf77f9eb
NuclMe <saltansergei@gmail.com>
2024-12-31 01:09:36 +0200

    added edit functionality

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,11 +1,12 @@
+using System.Security.Claims;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
-using API.Controllers;
-using AutoMapper;
-using API.DTOs;
+
 namespace API.Controllers;
 
 [Authorize]
@@ -16,17 +17,27 @@ public class UsersController(IUserRepository userRepository, IMapper mapper) : B
     {
         var users = await userRepository.GetMembersAsync();
 
-
         return Ok(users);
     }
 
-    [HttpGet("{username}")] //api/users/2
-    public async Task<ActionResult<MemberDto>> GetUsers(string username)
+    [HttpGet("{username}")]  // /api/users/2
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
         var user = await userRepository.GetMemberAsync(username);
 
         if (user == null) return NotFound();
 
         return user;
+    }
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (username == null) return BadRequest("No username found in token");
+        var user = await userRepository.GetUserByUsernameAsync(username);
+        if (user == null) return BadRequest("Could not find user");
+        mapper.Map(memberUpdateDto, user);
+        if (await userRepository.SaveAllAsync()) return NoContent();
+        return BadRequest("Failed to update the user");
     }
 }
