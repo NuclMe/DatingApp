cf47b37ed03d6c8a467157459542649604d6d01a
NuclMe <saltansergei@gmail.com>
2025-01-03 01:40:34 +0200

    added messages

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -10,6 +10,8 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
+import { memberDetailedResolver } from './_resolvers/member-detailed.resolver';
+
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   {
@@ -21,11 +23,15 @@ export const routes: Routes = [
       {
         path: 'members/:username',
         component: MemberDetailComponent,
+        resolve: { member: memberDetailedResolver },
+      },
+      {
+        path: 'member/edit',
+        component: MemberEditComponent,
         canDeactivate: [preventUnsavedChangesGuard],
       },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
-      { path: 'member/edit', component: MemberEditComponent },
     ],
   },
   { path: 'errors', component: TestErrorsComponent },
